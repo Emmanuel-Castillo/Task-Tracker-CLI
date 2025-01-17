e3c3bd02f76b884642664cb0f1f5f09c2f9aca7c
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-01-17 08:10:59 -0600

    Started project

---
+++ main.py
@@ -0,0 +1,11 @@
+# This application simulates a command line interface using Python
+# Users will be able to input commands and arguments that will then execute actions performed from
+# a traditional Task Tracker.
+# These actions involve:
+#   - Add, Update, Delete tasks
+#   - Mark a task as in progress or done
+#   - List all tasks
+#   - List all tasks that are done
+#   - List all tasks that are not done
+#   - List all tasks that are in progress
+
